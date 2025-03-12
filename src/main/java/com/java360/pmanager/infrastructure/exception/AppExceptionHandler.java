460fbcffca6ad13d689959b40bcc3001ff14b1da
antoniojunio <juniordsj@gmail.com>
2025-03-12 20:39:23 -0300

    refactor: Altera exceções do tipo RequestException

--- src/main/java/com/java360/pmanager/infrastructure/exception/AppExceptionHandler.java
+++ src/main/java/com/java360/pmanager/infrastructure/exception/AppExceptionHandler.java
@@ -9,24 +9,43 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = RequestException.class)
+    public ResponseEntity<Object> handleRequestException(RequestException ex, WebRequest request) {
+       return handleException(ex,ex.getErrorCode(),ex.getMessage(), BAD_REQUEST,request);
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) {
+        return handleException(ex, null, ex.getMessage(), INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    private ResponseEntity<Object> handleException(
+            Exception ex,
+            String errorCode,
+            String message,
+            HttpStatus status,
+            WebRequest request
+    ) {
         ServletWebRequest servletWebRequest = (ServletWebRequest) request;
 
         return handleExceptionInternal(
                 ex,
                 RestError
                         .builder()
-//                        .errorCode("XYZ")
-                        .errorMessage(ex.getMessage())
-                        .status(HttpStatus.BAD_REQUEST.value())
+                        .errorCode(errorCode)
+                        .errorMessage(message)
+                        .status(status.value())
                         .path(servletWebRequest.getRequest().getRequestURI())
                         .build(),
                 new HttpHeaders(),
-                HttpStatus.BAD_REQUEST,
+                status,
                 request
         );
     }
---
+++ src/main/java/com/java360/pmanager/infrastructure/exception/RequestException.java
@@ -0,0 +1,15 @@
+package com.java360.pmanager.infrastructure.exception;
+
+import lombok.Getter;
+
+@Getter
+public class RequestException extends RuntimeException {
+
+    private final String errorCode;
+
+    public RequestException(String message ,String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+}
